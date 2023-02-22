1d77bb407eba3aa30a093e6bcb2e4dc6774bb5cc
4rkal <4rkal@pm.me>
2023-02-22 14:20:05 +0200

    changed shit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button practical = (Button)findViewById(R.id.button2);
+        Button theoretical = (Button)findViewById(R.id.button);
+        practical.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Practical1.class );
+                startActivity(intent);
+            }
+        });
     }
 }
